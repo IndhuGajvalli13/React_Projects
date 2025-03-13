130613085ec3572b7bc99bb2a5b5b2906e3b4591
Indhu Gajvalli <141825420+IndhuGajvalli13@users.noreply.github.com>
2025-03-13 06:42:31 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,88 @@
+export const data = [
+    {
+        question:"Which of the following command is used to create react-js-app ?",
+        option1:"npx create-react-app appname",
+        option2:"npm install create-react-app",
+        option3:"npx install create-react-app -g",
+        option4:"install - l create-react-app",
+        ans:1,
+
+    },
+    {
+        question:"In React.js which one of the following is used to create a class for Inheritance ?",
+        option1:"Creates",
+        option2:"Extends",
+        option3:"Inherits",
+        option4:"Delete",
+        ans:2,
+        
+    },
+    {
+        question:"What is the default port number in which the application run ?",
+        option1:"3000",
+        option2:"8080",
+        option3:"5000",
+        option4:"3030",
+        ans:1,
+        
+    },
+    {
+        question:"Which of the following valid component return type of React ?",
+        option1:"2",
+        option2:"5",
+        option3:"1",
+        option4:"3",
+        ans:3,
+        
+    },
+    {
+        question:"Which of the following is a way to handle data in React.js ?",
+        option1:"State & Props",
+        option2:"Services & Components",
+        option3:"Services & Components",
+        option4:"State & Component",
+        ans:1,
+        
+    },
+    {
+        question:"Which of the following is must for the API in React.js ?",
+        option1:"SetinitialComponent",
+        option2:"renderComponent",
+        option3:"render",
+        option4:"All of the Above",
+        ans:2,
+    },
+    {
+        question:"Which of the following is true regarding Babel ?",
+        option1:"Compiler",
+        option2:"Transpilar",
+        option3:"Both of the Above",
+        option4:"None of the Above",
+        ans:3,
+    },
+    {
+        question:"In React.js, how we can pass the data from one component to another in React.js ?",
+        option1:"SetState",
+        option2:"Render with Arguments",
+        option3:"props",
+        option4:"propTypes",
+        ans:3,
+        
+    },
+    {
+        question:"Which of the following function is true about changing the state in React.js ?",
+        option1:"this.state {}",
+        option2:"this.SetState",
+        option3:"this.setChangeState",
+        option4:"All of the above",
+        ans:2,
+    },
+    {
+        question:"Which of the following method is true about referring parent class in React.js ?",
+        option1:"self()",
+        option2:"inherits()",
+        option3:"this()",
+        option4:"super()",
+        ans:4,
+    },
+];
